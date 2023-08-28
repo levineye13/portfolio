4e0ee00244c5cfb0b9c1210b641f0e52f8d6832d
Oleg Zharov <levineye@gmail.com>
2023-08-28 21:45:20 +0300

    feat: сверстать страницу контактов

---
+++ src/pages/contacts/index.tsx
@@ -0,0 +1,20 @@
+import React, { FC, ReactElement } from 'react';
+
+import Title from '../../components/section-title';
+import styles from './index.module.scss';
+
+const Contacts: FC = (): ReactElement => {
+  return (
+    <section className={styles.section}>
+      <Title>Contacts</Title>
+      <p className={styles.subtitle}>Location</p>
+      <p className={styles.info}>Moscow, Russia</p>
+      <p className={styles.subtitle}>Telegram / WhatsApp</p>
+      <p className={styles.info}>+7 (999) 982-31-27</p>
+      <p className={styles.subtitle}>Email</p>
+      <p className={`${styles.info} ${styles.email}`}>levineye@gmail.com</p>
+    </section>
+  );
+};
+
+export default Contacts;
