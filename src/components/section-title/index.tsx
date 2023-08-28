d61ad8c0619fb5b6c251a8460f6003d3bcd79b79
Oleg Zharov <levineye@gmail.com>
2023-08-28 13:14:39 +0300

    feat: добавить миксины в заголовок секции

--- src/components/section-title/index.tsx
+++ src/components/section-title/index.tsx
@@ -4,10 +4,14 @@ import styles from './index.module.scss';
 
 interface ISectionTitle {
   readonly children: string;
+  readonly className: string;
 }
 
-const SectionTitle: FC<ISectionTitle> = ({ children }): ReactElement => {
-  return <h2 className={styles.title}>{children}</h2>;
+const SectionTitle: FC<ISectionTitle> = ({
+  children,
+  className,
+}): ReactElement => {
+  return <h2 className={`${styles.title} ${className}`}>{children}</h2>;
 };
 
 export default SectionTitle;
