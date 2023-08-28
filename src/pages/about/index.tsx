a2784e2d4129b73cfe5a4d284545440d5dc683d0
Oleg Zharov <levineye@gmail.com>
2023-08-28 21:47:01 +0300

    feat: перенести страницу обо мне в pages

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,14 +1,17 @@
-import React, { ReactElement } from 'react';
+import React, { FC, ReactElement } from 'react';
 
+import Printer from '../../components/printer';
 import styles from './index.module.scss';
 
-const About = (): ReactElement => {
+const About: FC = (): ReactElement => {
   return (
     <section className={styles.section}>
       <h1 className={styles.title}>
         Hi, my name is <span className={styles.name}>Oleg</span>
       </h1>
-      <p className={styles.subtitle}>a frontend developer</p>
+      <p className={styles.subtitle}>
+        a frontend <Printer>developer</Printer>
+      </p>
       <p className={styles.description}>
         with passion for learning and creating.
       </p>
