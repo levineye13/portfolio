d751899c4e3a73ab9f44f7452ee541d6ad8ab55c
Oleg Zharov <levineye@gmail.com>
2023-08-29 17:53:27 +0300

    feat: добавить страницу со скилами

---
+++ src/pages/skills/index.tsx
@@ -0,0 +1,29 @@
+import React, { FC, ReactElement } from 'react';
+
+import Title from '../../components/section-title';
+import Skill from '../../components/skill';
+import { skills } from '../../data/skills';
+import styles from './index.module.scss';
+
+const Skills: FC = (): ReactElement => {
+  return (
+    <section className={styles.section}>
+      <Title>Skills</Title>
+      <p className={styles.subtitle}>Frontend</p>
+      <ul className={styles.list}>
+        {skills.frontend.map((skill) => (
+          <Skill {...skill} />
+        ))}
+        <li className={styles.item}></li>
+      </ul>
+      <p className={styles.subtitle}>Backend</p>
+      <ul className={styles.list}>
+        {skills.backend.map((skill) => (
+          <Skill {...skill} />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Skills;
