2ff925bbede24495739ef3d8384cda8ade2c5d19
Oleg Zharov <levineye@gmail.com>
2023-08-29 17:51:27 +0300

    feat: реэкспорт картинок скилов

---
+++ src/images/index.ts
@@ -0,0 +1,37 @@
+import html from './html.png';
+import css from './css.png';
+import sass from './sass.png';
+import js from './js.png';
+import redux from './redux.png';
+import react from './react.png';
+import ts from './typescript.png';
+import webpack from './webpack.png';
+import node from './nodejs.png';
+import express from './express.png';
+import mongoose from './mongoose.png';
+import sequelize from './sequelize.png';
+import http from './http.png';
+import restapi from './rest.png';
+import figma from './figma.png';
+import postman from './postman.png';
+import jest from './jest.png';
+
+export const images = {
+  html,
+  css,
+  sass,
+  js,
+  ts,
+  react,
+  redux,
+  webpack,
+  node,
+  express,
+  mongoose,
+  sequelize,
+  http,
+  restapi,
+  figma,
+  postman,
+  jest,
+};
