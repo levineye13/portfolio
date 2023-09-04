7986ab4b54a6837f299988c319bee9b96afb8487
Oleg Zharov <levineye@gmail.com>
2023-09-04 13:01:33 +0300

    feat: добавить картинки репозиториев

---
+++ src/core/data/repos.ts
@@ -0,0 +1,17 @@
+import Burger from '../../images/gitRepos/react-burger.png';
+import Mesto from '../../images/gitRepos/mesto.png';
+import Movies from '../../images/gitRepos/movies-explorer.png';
+import Travel from '../../images/gitRepos/russian-travel.png';
+import Vpn from '../../images/gitRepos/laslesvpn.png';
+import Project from '../../images/gitRepos/project-company.png';
+
+export const gitRepos = [
+  { name: 'react-burger', img: Burger },
+  { name: 'mesto-react', img: Mesto },
+  { name: 'react-mesto-api-full', img: Mesto },
+  { name: 'LaslesVPN', img: Vpn },
+  { name: 'project-company', img: Project },
+  { name: 'russian-travel', img: Travel },
+  { name: 'movies-explorer-frontend', img: Movies },
+  { name: 'movies-explorer-backend', img: Movies },
+];
