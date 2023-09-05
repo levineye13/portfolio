5a8ed434d9c8faba776c07c62c3ee0c499e41da7
Oleg Zharov <levineye@gmail.com>
2023-09-05 14:38:27 +0300

    feat: получать данные о репозиториях и выводить список

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -2,18 +2,28 @@ import React, { ReactElement } from 'react';
 
 import Project from '../project';
 import styles from './index.module.scss';
-import Img from '../../images/img.png';
+import { useAppSelector } from '../../core/store/hooks';
+import { gitRepos } from '../../core/data/repos';
 
 const Projects = (): ReactElement => {
+  const { projects } = useAppSelector((state) => state.project);
+
   return (
-    <div className={styles.div}>
-      <Project src={Img} title="Erundistika" />
-      <Project src={Img} title="Erundistika" />
-      <Project src={Img} title="Erundistika" />
-      <Project src={Img} title="Erundistika" />
-      <Project src={Img} title="Erundistika" />
-      <Project src={Img} title="Erundistika" />
-    </div>
+    <ul className={styles.list}>
+      {projects.map((project) => {
+        const rep = gitRepos.find((rep) => rep.name === project.name);
+
+        return (
+          <li className={styles.item} key={project.id}>
+            <Project
+              src={rep?.img || ''}
+              title={project.name}
+              link={project.html_url}
+            />
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
