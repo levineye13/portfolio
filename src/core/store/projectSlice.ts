c54e1244fb425bcf7be050e2f3aef85118287ae8
Oleg Zharov <levineye@gmail.com>
2023-09-05 14:39:42 +0300

    feat: добавить редакс для проектов

---
+++ src/core/store/projectSlice.ts
@@ -0,0 +1,34 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+import { getRepos } from '../githubApi';
+import { gitRepos } from '../data/repos';
+
+interface ProjectsState {
+  projects: Array<{ id: number; name: string; html_url: string }>;
+}
+
+const initialState: ProjectsState = {
+  projects: [],
+};
+
+export const getProjects = createAsyncThunk(
+  'projects/getProjects',
+  async (_, thunkAPI) => {
+    const res = await getRepos(gitRepos.map((rep) => rep.name));
+
+    return res;
+  }
+);
+
+export const projectsSlice = createSlice({
+  name: 'projects',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getProjects.fulfilled, (state, action) => {
+      state.projects = action.payload;
+    });
+  },
+});
+
+export default projectsSlice.reducer;
