2cb32146609df1ea898466875d65a5d306f2e4e7
Oleg Zharov <levineye@gmail.com>
2023-09-08 13:30:29 +0300

    feat: убрать лишние стили и добавить адаптив хедеру

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,55 +1,13 @@
 import React, { FC, ReactElement } from 'react';
-import { NavLink } from 'react-router-dom';
 
-import { APP_ROUTES } from '../../core/constants';
+import BurgerButton from '../burger-button';
 import styles from './index.module.scss';
-
-const { projects, skills, contacts } = APP_ROUTES;
-
-const checkActiveLink = (
-  isActive: boolean,
-  style: string,
-  activeStyle: string
-): string => (isActive ? `${style} ${activeStyle}` : style);
 
 const Header: FC = (): ReactElement => {
   return (
     <header className={styles.header}>
       <p className={styles.description}>Portfolio</p>
-      <nav className={styles.menu}>
-        <ul className={styles.list}>
-          <li className={styles.item}>
-            <NavLink
-              to={projects}
-              className={({ isActive }) =>
-                checkActiveLink(isActive, styles.link, styles.link_active)
-              }
-            >
-              Projects
-            </NavLink>
-          </li>
-          <li className={styles.item}>
-            <NavLink
-              to={skills}
-              className={({ isActive }) =>
-                checkActiveLink(isActive, styles.link, styles.link_active)
-              }
-            >
-              Skills
-            </NavLink>
-          </li>
-          <li className={styles.item}>
-            <NavLink
-              to={contacts}
-              className={({ isActive }) =>
-                checkActiveLink(isActive, styles.link, styles.link_active)
-              }
-            >
-              Contacts
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+      <BurgerButton />
     </header>
   );
 };
