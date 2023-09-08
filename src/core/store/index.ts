6e13e35b425638549237756ee08cef9b6719e98d
Oleg Zharov <levineye@gmail.com>
2023-09-08 18:52:32 +0300

    feat: добавить редакс для меню

--- src/core/store/index.ts
+++ src/core/store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import githubProjects from './projectSlice';
+import menu from './menuSlice';
 
 export const store = configureStore({
   reducer: {
     project: githubProjects,
+    menu,
   },
 });
 
