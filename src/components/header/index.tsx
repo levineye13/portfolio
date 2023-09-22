d547540ec52df33d93dfdc5fd023b85412ce3b2a
Oleg Zharov <levineye@gmail.com>
2023-09-22 12:34:30 +0300

    feat: добавить стили бургеру

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,13 @@
 import React, { FC, ReactElement } from 'react';
 
-import BurgerButton from '../burger-button';
+import Menu from '../menu';
 import styles from './index.module.scss';
 
 const Header: FC = (): ReactElement => {
   return (
     <header className={styles.header}>
       <p className={styles.description}>Portfolio</p>
-      <BurgerButton />
+      <Menu />
     </header>
   );
 };
