77fce713a5d2035610f73bdae8f13f0375baf5dd
Oleg Zharov <levineye@gmail.com>
2023-09-24 21:19:39 +0300

    feat: заполнить события об учебе

---
+++ src/core/data/timeline.ts
@@ -0,0 +1,37 @@
+type event = { year: number; description: string };
+
+export const timeline: event[] = [
+  { year: 2018, description: 'Entered the University' },
+  {
+    year: 2020,
+    description:
+      'Became interested in web development and started learning page layout',
+  },
+  {
+    year: 2021,
+    description:
+      'Completed the Yandex.Practicum course on web development (10 months)',
+  },
+  {
+    year: 2021,
+    description: "Earned a bachelor's degree (4 years)",
+  },
+  {
+    year: 2021,
+    description:
+      'Completed the Yandex.Practicum course on algorithms and data structures (4 months)',
+  },
+  {
+    year: 2022,
+    description:
+      'Completed the Yandex.Practicum course on react development (3 months)',
+  },
+  {
+    year: 2022,
+    description: 'Graduated from the military department',
+  },
+  {
+    year: 2023,
+    description: "Earned a master's degree",
+  },
+];
