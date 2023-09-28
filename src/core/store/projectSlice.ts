2dc4f50b39451e05556d9ecdc73f8b9da64cf34e
Oleg Zharov <levineye@gmail.com>
2023-09-28 13:43:37 +0300

    feat: обработать состояние ответа апи

--- src/core/store/projectSlice.ts
+++ src/core/store/projectSlice.ts
@@ -5,18 +5,25 @@ import { gitRepos } from '../data/repos';
 
 interface IProjectsState {
   projects: Array<{ id: number; name: string; html_url: string }>;
+  loading: boolean;
+  error: string | null;
 }
 
 const initialState: IProjectsState = {
   projects: [],
+  loading: false,
+  error: null,
 };
 
 export const getProjects = createAsyncThunk(
   'projects/getProjects',
   async (_, thunkAPI) => {
-    const res = await getRepos(gitRepos.map((rep) => rep.name));
-
-    return res;
+    try {
+      const res = await getRepos(gitRepos.map((rep) => rep.name));
+      return res;
+    } catch (e) {
+      return thunkAPI.rejectWithValue('Failed to fetch projects.');
+    }
   }
 );
 
@@ -25,9 +32,23 @@ export const projectsSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(getProjects.fulfilled, (state, action) => {
-      state.projects = action.payload;
-    });
+    builder
+      .addCase(getProjects.pending, (state, action) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getProjects.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.projects = action.payload;
+      })
+      .addCase(getProjects.rejected, (state, action) => {
+        state.loading = false;
+        state.error = JSON.stringify({
+          code: action.error.code || '',
+          message: action.error.message || new Error('Error to fetch projects'),
+        });
+      });
   },
 });
 
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,14 +1,18 @@
-import React, { ReactElement } from 'react';
+import React, { FC, ReactElement } from 'react';
 
 import Title from '../../components/section-title';
 import ProjectList from '../../components/projects';
+import Loader from '../../components/loader';
 import styles from './index.module.scss';
+import { useAppSelector } from '../../core/store/hooks';
 
-const Projects = (): ReactElement => {
+const Projects: FC = (): ReactElement => {
+  const { loading } = useAppSelector((state) => state.project);
+
   return (
     <section className={styles.section}>
       <Title className={styles.title}>Projects</Title>
-      <ProjectList />
+      {loading ? <Loader /> : <ProjectList />}
     </section>
   );
 };
