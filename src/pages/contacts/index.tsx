7630834c0c7f36cdc8fde187e4f1bf61d95eb948
Oleg Zharov <levineye@gmail.com>
2023-10-20 13:50:10 +0300

    fix: поправить шрифт

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -10,7 +10,7 @@ const Contacts: FC = (): ReactElement => {
       <p className={styles.subtitle}>Location</p>
       <p className={styles.info}>Moscow, Russia</p>
       <p className={styles.subtitle}>Telegram / WhatsApp</p>
-      <p className={styles.info}>+7 (999) 982-31-27</p>
+      <p className={`${styles.info} ${styles.tel}`}>+7 (999) 982-31-27</p>
       <p className={styles.subtitle}>Email</p>
       <p className={`${styles.info} ${styles.email}`}>levineye@gmail.com</p>
     </section>
