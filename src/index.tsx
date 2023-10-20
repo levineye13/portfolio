fc46a17293699d7fa33f7c955f2de05b068c50e8
Oleg Zharov <levineye@gmail.com>
2023-10-20 13:34:00 +0300

    feat: добавить базовый путь для gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio">
       <Provider store={store}>
         <App />
       </Provider>
